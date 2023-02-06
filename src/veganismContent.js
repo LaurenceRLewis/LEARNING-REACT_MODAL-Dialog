287ee90af7d6b53b5a52bbb04109429aeadeabf8
Laurence Lewis (he/him) <laurence@a11ycats.com.au>
2023-02-07 09:14:36 +1000

    Fix veganismContent.js

---
+++ src/veganismContent.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+export const VeganismContent = () => {
+  return (
+    <div>
+      <p>
+        Veganism is a lifestyle and dietary choice that seeks to exclude the use
+        of animals for food, clothing, or any other purpose. Adopting a vegan
+        lifestyle means avoiding meat, dairy, eggs, and other animal products,
+        as well as products that have been tested on animals.
+      </p>
+      <p>
+        One of the primary reasons for going vegan is to reduce animal
+        suffering. By choosing not to consume animal products, vegans are
+        advocating for a more humane and compassionate world. In addition,
+        veganism has been linked to numerous health benefits, such as a reduced
+        risk of heart disease, certain types of cancer, and obesity. It is also
+        a more environmentally sustainable choice, as animal agriculture is a
+        significant contributor to greenhouse gas emissions and deforestation.
+      </p>
+    </div>
+  );
+};
